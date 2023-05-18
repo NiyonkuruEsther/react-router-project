f8a871495bde37958894617ce3670f469a91f9e7
estherniyo <estherniyonkuru123@gmail.com>
2023-05-18 09:32:11 +0200

    worked on the error with deployment on the server.js file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,14 @@
-export async function getVans() {
-  const res = await fetch("/api/vans");
+export async function getVans(id) {
+  const url = id ? `/api/vans/${id}` : "/api/vans";
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw {
+      message: "Failed to fetch vans",
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
   const data = await res.json();
+  console.log(data);
   return data.vans;
 }
